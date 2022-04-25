4e60faceb35545f7222e56962d1498aad24b057d
Bertho <berthojoris@gmail.com>
2022-04-25 14:28:23 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Staterpack App",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null).then(
+    (_) => runApp(
+      GetMaterialApp(
+        title: "Staterpack App",
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+      ),
     ),
   );
 }
